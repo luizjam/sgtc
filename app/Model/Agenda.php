2c19e312d06364fc6e23f096cbe0b8ced2b7aa28
luizcarlos.fonseca <luizcarlos.fonseca@CODESP-1855.APS.TI>
2015-02-12 17:58:39 -0300

    configurando conexão com o banco oracle adicionar o vendor razorflow

---
+++ app/Model/Agenda.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Agenda
+ *
+ * @author luizcarlos.fonseca
+ */
+class Agenda extends AppModel{
+    //put your code here
+    public $name = 'Agenda';
+    public $useTable = 'tb_agenda';
+    public $primariKey = 'id_agenda';
+}
